70e33a48152e5b2be2ca961923ea2cafed3e8fd8
marpercor8 <mario.p.c.2001@gmail.com>
2022-03-09 12:24:28 +0100

    Task-031: Done.

---
+++ src/main/java/acme/roles/Inventor.java
@@ -0,0 +1,36 @@
+
+package acme.roles;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+import acme.framework.roles.UserRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Inventor extends UserRole {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	protected String			company;
+
+	@NotBlank
+	protected String			statement;
+	
+	
+	protected String			link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
