e7118020b50e7ab394495636fd256bb91353cdaa
mariopc10 <73860635+marpercor8@users.noreply.github.com>
2022-03-18 10:55:58 +0100

    Task-048: Done.

--- src/main/java/acme/entities/component/Component.java
+++ src/main/java/acme/entities/component/Component.java
@@ -1,80 +1,20 @@
 package acme.entities.component;
 
-
-import java.util.List;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.URL;
-
-import acme.entities.tool.Tool;
-import acme.entities.toolkit.Toolkit;
-import acme.framework.datatypes.Money;
-import acme.framework.entities.AbstractEntity;
+import acme.entities.artefact.Artefact;
 import lombok.Getter;
 import lombok.Setter;
+
 
 @Getter
 @Setter
 @Entity
-public class Component extends AbstractEntity{
+public class Component extends Artefact{
 	
 	// Serialisation identifier -----------------------------------------------
 
 		protected static final long	serialVersionUID	= 1L;
-		
-	// Attributes -------------------------------------------------------------	
-		
-		@NotBlank
-		@Length(max=101)
-		protected String name;
-		
-		@NotBlank
-		@Length(max=101)
-		protected String technology;
-		
-		@Pattern(regexp = "^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
-		@Column(unique =true)
-		protected String code;
-		
-		@NotBlank
-		@Length(max=256)
-		protected String description;
 
-		@Valid
-		protected Money retailPrice;
-		
-		@URL
-		protected String moreInfo;
-		
-		// Derived attributes -----------------------------------------------------
-
-		// Relationships ----------------------------------------------------------
-
-		@OneToMany
-		@JoinTable(name= "to_work_with",
-		joinColumns = {@JoinColumn(name= "fk_component1")},
-		inverseJoinColumns = {@JoinColumn(name= "fk_component2")})
-		protected List<Component> toWorkWith;
-
-		@Valid
-		@NotNull
-        @ManyToOne(optional=false)
-		protected Tool Tool;
-
-		@NotNull
-		@Valid
-		@ManyToOne(optional = false)
-		protected Toolkit toolkit;
 }	
 
--- src/main/java/acme/entities/tool/Tool.java
+++ src/main/java/acme/entities/tool/Tool.java
@@ -1,52 +1,27 @@
 package acme.entities.tool;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.NotNull;
 
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.URL;
-
-import acme.framework.datatypes.Money;
-import acme.framework.entities.AbstractEntity;
+import acme.entities.artefact.Artefact;
+import acme.entities.toolkit.Toolkit;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Tool extends AbstractEntity{
+public class Tool extends Artefact{
 
 	// Serialisation identifier -----------------------------------------------
 
 	protected static final long	serialVersionUID	= 1L;
 
-	// Attributes -------------------------------------------------------------
-	
-	@NotBlank
-	@Length(max = 101)
-	protected String name;
-	
-	@Column(unique = true)
-	@Pattern(regexp = "^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
-	protected String code;
-
-	
-	@NotBlank
-	@Length(max = 101)
-	protected String technology;
-	
-	@NotBlank
-	@Length(max = 256)
-	protected String description;
-	
+	@NotNull
 	@Valid
-	protected Money retailPrice;
-	
-	@URL
-	protected String moreInfo;
-	
+	@ManyToOne(optional=false) 
+	protected Toolkit toolkit;
 	
 }
