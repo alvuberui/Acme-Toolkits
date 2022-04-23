bdf3e0b80f702ebe5265186db8ab3ed94a2a5262
Mario Rodríguez <marrodgar62@alum.us.es>
2022-04-23 13:02:10 +0200

    Tests toolkits

--- src/test/java/acme/testing/inventor/toolkits/InventorToolkitsListTest.java
+++ src/test/java/acme/testing/inventor/toolkits/InventorToolkitsListTest.java
@@ -36,9 +36,88 @@ public class InventorToolkitsListTest extends TestHarness{
 		super.checkInputBoxHasValue("published", published);
 		
 		super.signOut();
-
-
-
 	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/inventor/Toolkits/artefacts-list.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20)
+	public void positiveToolInventorTest(final int recordIndex, final String type, final String name, final String code, final String techonology, final String description, final String retailPrice, final String moreInfo) {
+		super.signIn("inventor1", "inventor1");
+		
+		super.clickOnMenu("Inventor", "My toolkit");
+		super.checkListingExists();
+		super.sortListing(0, "desc");		
+		super.clickOnListingRecord(recordIndex);
+		super.clickOnButton("Artefacts");
+
+		super.checkColumnHasValue(recordIndex, 0, type);
+		super.checkColumnHasValue(recordIndex, 1, name);
+		super.checkColumnHasValue(recordIndex, 2, code);
+		super.checkColumnHasValue(recordIndex, 3, techonology);
+		super.checkColumnHasValue(recordIndex, 4, description);
+		super.checkColumnHasValue(recordIndex, 5, retailPrice);
+		
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		super.checkInputBoxHasValue("type", type);
+		super.checkInputBoxHasValue("name", name);
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("technology", techonology);
+		super.checkInputBoxHasValue("description", description);
+		super.checkInputBoxHasValue("retailPrice", retailPrice);
+		
+		super.signOut();
+	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/inventor/Toolkits/artefacts-list.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(30)
+	public void NegativeToolInventorTest(final int recordIndex) {
+		super.signIn("inventor1", "inventor1");
+		
+		super.clickOnMenu("Inventor", "My toolkit");
+		super.clickOnListingRecord(recordIndex);
+		super.clickOnButton("Artefacts");
+		final String query = super.getCurrentQuery();
+
+		super.signOut();
+
+		
+		super.signIn("inventor2", "inventor2");
+
+		final String queryNueva = query.substring(1);
+		System.out.println(query);
+		super.navigate("/inventor/artefact/list-artefact-toolkit",queryNueva);
+		super.checkPanicExists();
+
+		super.signOut();
+		
+	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/inventor/Toolkits/list-toolkits.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(40)
+	public void NegativeToolkitInventorTest(final int recordIndex) {
+		super.signIn("inventor1", "inventor1");
+		
+		super.clickOnMenu("Inventor", "My toolkit");
+		super.checkListingExists();
+		super.clickOnListingRecord(recordIndex);
+		final String query = super.getCurrentQuery();
+
+		super.signOut();
+
+		
+		super.signIn("inventor2", "inventor2");
+
+		final String queryNueva = query.substring(1);
+		super.navigate("/inventor/toolkit/show",queryNueva);
+		super.checkPanicExists();
+
+		super.signOut();
+		
+	}
+	
+
 	
 }
