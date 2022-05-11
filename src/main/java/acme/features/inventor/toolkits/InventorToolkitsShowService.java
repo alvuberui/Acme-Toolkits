5185f1fc0ae13c2515a837773c84d31ea09fd814
Mario Rodríguez <marrodgar62@alum.us.es>
2022-05-11 11:27:45 +0200

    Arreglados fallos

--- src/main/java/acme/features/inventor/toolkits/InventorToolkitsShowService.java
+++ src/main/java/acme/features/inventor/toolkits/InventorToolkitsShowService.java
@@ -4,19 +4,15 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import acme.components.ExchangeRate;
 import acme.components.ExchangeService;
 import acme.entities.artefact.Artefact;
 import acme.entities.systemConfiguration.SystemConfiguration;
 import acme.entities.toolkit.Toolkit;
-import acme.forms.MoneyExchange;
 import acme.framework.components.models.Model;
 import acme.framework.controllers.Request;
 import acme.framework.datatypes.Money;
 import acme.framework.entities.Principal;
-import acme.framework.helpers.StringHelper;
 import acme.framework.services.AbstractShowService;
 import acme.roles.Inventor;
 
@@ -87,16 +83,18 @@ public class InventorToolkitsShowService implements AbstractShowService<Inventor
 		model.setAttribute("toolkitId", entity.getId());
 		if(systemConfiguration != null && systemConfiguration.getCurrency() != null) {
 			if(!artefacts.isEmpty()) {
-				final Double price = artefacts.stream().map(x -> exchangeService.exchangeMoney(x.getRetailPrice()).getAmount()).reduce(0.0, (a, b) -> a + b);
+				final Double price = artefacts.stream().map(x -> this.exchangeService.exchangeMoney(x.getRetailPrice()).getAmount()).reduce(0.0, (a, b) -> a + b);
 				final Money money =  new Money();
 				money.setAmount(price);
 				money.setCurrency(systemConfiguration.getCurrency());
 				model.setAttribute("price",money);
+				model.setAttribute("moneyExchange", this.exchangeService.exchangeMoney(money));
 			}else {
 				final Money money =  new Money();
 				money.setAmount(0.0);
 				money.setCurrency(systemConfiguration.getCurrency());
 				model.setAttribute("price",money);
+				model.setAttribute("moneyExchange", this.exchangeService.exchangeMoney(money));
 			}
 		}
 	}
