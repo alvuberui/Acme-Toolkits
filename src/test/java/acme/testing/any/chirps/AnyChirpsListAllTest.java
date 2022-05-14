b71dc92dc418a1d5a01582bf998d1025cad7a74f
Mario Rodríguez <marrodgar62@alum.us.es>
2022-05-14 15:38:43 +0200

    Test chirps fecha siempre buena corregido

--- src/test/java/acme/testing/any/chirps/AnyChirpsListAllTest.java
+++ src/test/java/acme/testing/any/chirps/AnyChirpsListAllTest.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-import acme.testing.TestHarness;
+import acme.testing.TemporalAwareTestHarness;
 
-public class AnyChirpsListAllTest extends TestHarness{
+public class AnyChirpsListAllTest extends TemporalAwareTestHarness{
 
 	
 	//Test cases ---------------------------------------------------
@@ -14,12 +14,15 @@ public class AnyChirpsListAllTest extends TestHarness{
 	@ParameterizedTest
 	@CsvFileSource(resources = "/any/chirps/list-chirps.csv", encoding="utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void positiveChirpsAnoymousTest(final int recordIndex, final String creationMoment, final String title, final String author, final String body, final String email) {
+	public void positiveChirpsAnoymousTest(final int recordIndex, final int deltaDays, final String title, final String author, final String body, final String email) {
+		String moment;
+		moment = super.computeDeltaMoment(deltaDays);
+		
 		super.clickOnMenu("Anonymous", "Chirp List");
 		super.checkListingExists();
-		super.sortListing(0, "asc");
+		super.sortListing(0, "desc");
 		
-		super.checkColumnHasValue(recordIndex, 0, creationMoment);
+		super.checkColumnHasValue(recordIndex, 0, moment);
 		super.checkColumnHasValue(recordIndex, 1, title);
 		super.checkColumnHasValue(recordIndex, 2, author);
 		super.checkColumnHasValue(recordIndex, 3, body);
@@ -30,15 +33,17 @@ public class AnyChirpsListAllTest extends TestHarness{
 	@ParameterizedTest
 	@CsvFileSource(resources = "/any/chirps/list-chirps.csv", encoding="utf-8", numLinesToSkip = 1)
 	@Order(20)
-	public void positiveChirpsInventorTest(final int recordIndex, final String creationMoment, final String title, final String author, final String body, final String email) {
+	public void positiveChirpsInventorTest(final int recordIndex, final int deltaDays, final String title, final String author, final String body, final String email) {
+		String moment;
+		moment = super.computeDeltaMoment(deltaDays);
 		
 		super.signIn("inventor1", "inventor1");
 
 		super.clickOnMenu("Authenticated", "Chirps List");
 		super.checkListingExists();
-		super.sortListing(0, "asc");
+		super.sortListing(0, "desc");
 		
-		super.checkColumnHasValue(recordIndex, 0, creationMoment);
+		super.checkColumnHasValue(recordIndex, 0, moment);
 		super.checkColumnHasValue(recordIndex, 1, title);
 		super.checkColumnHasValue(recordIndex, 2, author);
 		super.checkColumnHasValue(recordIndex, 3, body);
@@ -51,15 +56,17 @@ public class AnyChirpsListAllTest extends TestHarness{
 	@ParameterizedTest
 	@CsvFileSource(resources = "/any/chirps/list-chirps.csv", encoding="utf-8", numLinesToSkip = 1)
 	@Order(30)
-	public void positiveChirpsPatronTest(final int recordIndex, final String creationMoment, final String title, final String author, final String body, final String email) {
+	public void positiveChirpsPatronTest(final int recordIndex, final int deltaDays, final String title, final String author, final String body, final String email) {
+		String moment;
+		moment = super.computeDeltaMoment(deltaDays);
 		
 		super.signIn("patron1", "patron1");
 
 		super.clickOnMenu("Authenticated", "Chirps List");
 		super.checkListingExists();
-		super.sortListing(0, "asc");
+		super.sortListing(0, "desc");
 		
-		super.checkColumnHasValue(recordIndex, 0, creationMoment);
+		super.checkColumnHasValue(recordIndex, 0, moment);
 		super.checkColumnHasValue(recordIndex, 1, title);
 		super.checkColumnHasValue(recordIndex, 2, author);
 		super.checkColumnHasValue(recordIndex, 3, body);
@@ -72,15 +79,17 @@ public class AnyChirpsListAllTest extends TestHarness{
 	@ParameterizedTest
 	@CsvFileSource(resources = "/any/chirps/list-chirps.csv", encoding="utf-8", numLinesToSkip = 1)
 	@Order(40)
-	public void positiveChirpsAdministratorTest(final int recordIndex, final String creationMoment, final String title, final String author, final String body, final String email) {
+	public void positiveChirpsAdministratorTest(final int recordIndex, final int deltaDays, final String title, final String author, final String body, final String email) {
+		String moment;
+		moment = super.computeDeltaMoment(deltaDays);
 		
 		super.signIn("administrator", "administrator");
 
 		super.clickOnMenu("Authenticated", "Chirps List");
 		super.checkListingExists();
-		super.sortListing(0, "asc");
+		super.sortListing(0, "desc");
 		
-		super.checkColumnHasValue(recordIndex, 0, creationMoment);
+		super.checkColumnHasValue(recordIndex, 0, moment);
 		super.checkColumnHasValue(recordIndex, 1, title);
 		super.checkColumnHasValue(recordIndex, 2, author);
 		super.checkColumnHasValue(recordIndex, 3, body);
